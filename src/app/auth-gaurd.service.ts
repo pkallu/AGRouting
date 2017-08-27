5fd0452141104d3c3e8e994cf21dfe46441b2403
Pragathi Kallu <pkallu@univ-wea.com>
2017-08-26 22:02:25 -0500

    first commit

---
+++ src/app/auth-gaurd.service.ts
@@ -0,0 +1,12 @@
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+
+export class AuthGaurd implements CanActivate {
+canActivate(route: ActivatedRouteSnapshot,
+            state: RouterStateSnapshot)
+
+
+
+  constructor() {
+
+  }
+}
